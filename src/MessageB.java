f44cabbef099cd13d3054475064fc81132431039
victorGuyenne <victor.guyenne@gmail.com>
2025-11-17 16:08:36 +0100

    ajout messageB

---
+++ src/MessageB.java
@@ -0,0 +1,10 @@
+//création de la classe message b
+public class MessageB {
+
+	public static void main(String[] args) {
+		//affiche le message
+		System.out.println("Message du membre B");
+
+	}
+
+}
