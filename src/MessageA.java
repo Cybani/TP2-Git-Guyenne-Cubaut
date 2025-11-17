551f566519f3a8bb05285a82dc5850ee23ead5c8
Baptiste Cubaut <baptistecubaut@orange.fr>
2025-11-17 16:16:32 +0100

    Ajout d'une deuxième ligne au message de A

--- src/MessageA.java
+++ src/MessageA.java
@@ -2,7 +2,8 @@
 public class MessageA {
 
 	public static void main(String[] args) {
-		System.out.println("Message du membre A");
+		System.out.println("Message du membre A");//affiche le message du membre
+		System.out.println("Deuxième ligne de A");//affiche la deuxieme ligne de A
 
 	}
 
